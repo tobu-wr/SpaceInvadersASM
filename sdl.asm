66c3b99c802934d59e083cc4cc6947bd8366ed21
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-06-15 22:44:14 +0100

    add SDL_LoadBMP routine

--- sdl.asm
+++ sdl.asm
@@ -3,6 +3,7 @@
 
 extern SDL_Init, SDL_Quit, SDL_CreateWindow, SDL_DestroyWindow
 extern SDL_CreateRenderer, SDL_DestroyRenderer
+extern SDL_RWFromFile, SDL_LoadBMP_RW
 extern SDL_PollEvent
 
 struc SDL_Event
@@ -13,5 +14,20 @@ endstruc
 SDL_INIT_VIDEO: equ 0x20
 SDL_WINDOWPOS_UNDEFINED: equ 0x1fff0000
 SDL_QUIT: equ 0x100
+
+section .text
+SDL_LoadBMP:
+    sub rsp, 40
+    mov rdx, SDL_LoadBMP_mode ; mode
+    call SDL_RWFromFile
+    mov rcx, rax ; src
+    mov edx, 1 ; freesrc
+    call SDL_LoadBMP_RW
+    add rsp, 40
+    ret
+
+section .data
+SDL_LoadBMP_mode:
+    db "rb", 0
 
 %endif ; SDL_ASM
