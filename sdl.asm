e10c993e9d255d4d8a3703dd2cc9066fe59fadc0
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-06-15 18:58:26 +0100

    add sdl.asm

---
+++ sdl.asm
@@ -0,0 +1,9 @@
+%ifndef SDL_ASM
+%define SDL_ASM
+
+extern SDL_Init, SDL_Quit, SDL_CreateWindow, SDL_DestroyWindow, SDL_PollEvent
+
+SDL_INIT_VIDEO: equ 0x20
+SDL_WINDOWPOS_UNDEFINED: equ 0x1fff0000
+
+%endif ; SDL_ASM
