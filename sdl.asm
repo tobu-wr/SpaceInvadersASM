f9db2270c5fce420f52e7f8bb37a27867f7fedbe
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-06-15 19:26:29 +0100

    update sdl.asm

--- sdl.asm
+++ sdl.asm
@@ -3,7 +3,13 @@
 
 extern SDL_Init, SDL_Quit, SDL_CreateWindow, SDL_DestroyWindow, SDL_PollEvent
 
-SDL_INIT_VIDEO: equ 0x00000020
+struc SDL_Event
+    .type: resd 1
+    .padding: resb 52
+endstruc
+
+SDL_INIT_VIDEO: equ 0x20
 SDL_WINDOWPOS_UNDEFINED: equ 0x1fff0000
+SDL_QUIT: equ 0x100
 
 %endif ; SDL_ASM
