02c971929c082159e5d3923e594f9f0e0cffabce
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-06-19 17:11:49 +0100

    update sdl.asm for audio

--- src/sdl.asm
+++ src/sdl.asm
@@ -3,8 +3,8 @@
 
 extern SDL_Init, SDL_Quit, SDL_CreateWindow, SDL_DestroyWindow, SDL_CreateRenderer, SDL_DestroyRenderer, SDL_RenderSetLogicalSize
 extern IMG_Init, IMG_Quit, IMG_Load, SDL_FreeSurface, SDL_CreateTextureFromSurface, SDL_DestroyTexture
-extern SDL_PollEvent, SDL_GetKeyboardState, SDL_RenderCopy, SDL_RenderPresent
-extern SDL_GetTicks, SDL_Delay
+extern SDL_PollEvent, SDL_GetKeyboardState, SDL_RenderCopy, SDL_RenderPresent, SDL_GetTicks, SDL_Delay
+extern Mix_OpenAudio, Mix_CloseAudio, Mix_LoadWAV, Mix_FreeChunk, Mix_PlayChannel
 
 struc SDL_Event
     .type: resd 1
@@ -18,8 +18,10 @@ struc SDL_Rect
     .h: resd 1
 endstruc
 
+SDL_INIT_AUDIO: equ 0x10
 SDL_INIT_VIDEO: equ 0x20
-SDL_WINDOWPOS_UNDEFINED: equ 0x1fff_0000
+SDL_WINDOWPOS_UNDEFINED: equ 0x1fff0000
+AUDIO_U8: equ 8
 SDL_QUIT: equ 0x100
 SDL_SCANCODE_SPACE: equ 44
 SDL_SCANCODE_RIGHT: equ 79
